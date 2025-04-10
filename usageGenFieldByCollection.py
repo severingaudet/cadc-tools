d68a1b0709953825245667e21f96d2c73a90964f
Séverin Gaudet <severin.gaudet@nrc-cnrc.gc.ca>
2025-04-10 08:20:14 -0700

    First commit.

---
+++ usageGenFieldByCollection.py
@@ -0,0 +1,41 @@
+# This script is used to generate CSV files per collection containing the fields and counts of null values for all
+# fields in caom2.Observation and caom2.Plane tables.
+
+import pandas as pd
+import os.path
+
+if __name__ == "__main__":
+
+## Initialize the data structure for pre-generated collection counts for observations.
+    obs_filename = "config/collectionTotalObs.csv"
+    array_collection_obs = pd.read_csv(obs_filename)
+
+## Initialize lists of fields to be checked for null values.
+
+    fields_filename = "config/fieldNames.csv"
+    field_names = pd.read_csv(fields_filename)
+
+## Now loop through the the list of collections and process them
+
+    for index, row in array_collection_obs.iterrows():
+        collection = row['collection']
+        print(f"Processing collection {collection}")
+        collection_filename = f"fieldByCollection/{collection}.csv"
+        with open(collection_filename, 'w') as f:
+           f.write("Field, num_null, num_instances,percentage_null\n")
+
+        ## loop through all the fields
+
+        for index, row in field_names.iterrows():
+            field = row['field_name']
+            field_filename = f"collectionByField/{field}.csv"
+            if os.path.isfile(field_filename):
+                results = pd.read_csv(field_filename)
+                row = results.loc[results['collection'] == collection]
+                if len(row) >  0:
+                    num_null = row['num_null'].values[0]
+                    num_instances = row['num_instances'].values[0]
+                    percentage_null = row['percentage_null'].values[0]
+
+                    with open(collection_filename, 'a') as f:
+                        f.write(f"{field},{num_null},{num_instances},{percentage_null}\n")
